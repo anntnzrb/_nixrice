93296de3fead2be43df04af3d8c043d0e98fbdd6
anntnzrb <anntnzrb@protonmail.com>
2020-12-12 23:17:37 -0500

    GNU/Emacs config reboot

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,7 +1,5 @@
-;; literate config
+;;; init.el --- GNU/Emacs Configuration -*- lexical-binding: t; -*-
+
+;; literate configuration file
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.config/emacs/readme.org"))
-
-;; disable emacs settings up custom variables
-(unless (setq custom-file "/dev/null"))
-(unless custom-file (expand-file-name (setq custom-file "~/.cache")))
