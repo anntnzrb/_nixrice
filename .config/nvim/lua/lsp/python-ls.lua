8cf0acc093fb39f057321ff5bcbd71243aac7439
anntnzrb <anntnzrb@protonmail.com>
2021-06-01 12:28:56 -0500

    old files cleanup

--- .config/nvim/lua/lsp/python-ls.lua
+++
@@ -1,3 +0,0 @@
--- python-ls.lua --- LSP configurations for Python
-
-require'lspconfig'.pyright.setup{ on_attach = require'lsp'.common_on_attach }
--- .config/nvim/lua/plug-cfgs/kommentary.lua
+++
@@ -1,7 +0,0 @@
--- kommentary.lua --- kommentary plugin configurations
-
-vim.g.kommentary_create_default_mappings = false
-
--- mappings
-set_keymap('n', '<Leader>/', '<Plug>kommentary_line_default', {})
-set_keymap('v', '<Leader>/', '<Plug>kommentary_visual_default', {})
