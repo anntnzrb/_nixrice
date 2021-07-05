461d57c38b53e500aa13ed06ab87144426c70197
anntnzrb <anntnzrb@protonmail.com>
2021-07-04 19:32:49 -0500

    Font setup scripts (broken)

---
+++ .config/scripts/fonts/font-apl.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# font-apl.sh --- APL font
+
+# shellcheck disable=SC1091
+# follow sourced files
+
+. ./font-utils.sh
+
+# *****************************************************************************
+# Variables (constants)
+# *****************************************************************************
+
+font_link='https://github.com/abrudz/APL386'
+font_path="/tmp/font-${font_link##*/}"
+
+# *****************************************************************************
+# Main
+# *****************************************************************************
+
+# get font
+fgclone "${font_link}" "${font_path}"
+
+# copy (install) it
+install_font "${font_path}/APL386.ttf" || die "Error installing fonts..."
+
+# update fonts
+update_fonts
