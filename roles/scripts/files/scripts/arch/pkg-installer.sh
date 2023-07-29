54172ff33829f274f5d68744fe21a33ebbfa7a42
anntnzrb <anntnzrb@protonmail.com>
2023-07-29 12:27:11 -0500

    refactor(arch): use full db name for pkgs

--- roles/scripts/files/scripts/arch/pkg-installer.sh
+++ roles/scripts/files/scripts/arch/pkg-installer.sh
@@ -24,36 +24,29 @@ check_requirements() {
         printf 'Error: This script requires pacman but it is not installed. Exiting...\n' >&2
         exit 1
     }
+
+    test ! -x "$(command -v ${aur_helper})" && {
+        printf 'Error: This script requires %s but it is not installed. Exiting...\n' "${aur_helper}" >&2
+        exit 1
+    }
 }
 
 parse_pkgs() {
-    pacman_pkgs_file=$(mktemp)
     aur_pkgs_file=$(mktemp)
 
     tmp_prog_file=$(mktemp)
     # trim comments and empty lines
     sed '/^#/d ; /^$/d' < "${pkgs_file}" > "${tmp_prog_file}"
 
-    while IFS=',' read -r pkgs_type pkgs_name
-    do
-        case "${pkgs_type}" in
-            P) printf "%s\n" "${pkgs_name}" >> "${pacman_pkgs_file}" ;;
-            A) printf "%s\n" "${pkgs_name}" >> "${aur_pkgs_file}" ;;
-            *) printf "Unknown pkgs type for pkgs: %s. Skipping...\n" "${pkgs_name}" ;;
-        esac
-    done < "${tmp_prog_file}"
-
-    install_pkgs "${pacman_pkgs_file}" "${aur_pkgs_file}"
-    rm -f "${tmp_prog_file}" "${pacman_pkgs_file}" "${aur_pkgs_file}"
+    install_pkgs "${tmp_prog_file}"
+    rm -f "${tmp_prog_file}" "${aur_pkgs_file}"
 }
 
 install_pkgs() {
-    pacman_pkgs_file="${1}"
-    aur_pkgs_file="${2}"
-    common_flags="-S --noconfirm --needed"
+    aur_pkgs_file="${1}"
+    flags="--noconfirm --needed -Syyu"
 
-    test -s "${pacman_pkgs_file}" && sudo pacman ${common_flags} - < "${pacman_pkgs_file}"
-    test -s "${aur_pkgs_file}" && "${aur_helper}" ${common_flags} - < "${aur_pkgs_file}"
+    test -s "${aur_pkgs_file}" && "${aur_helper}" ${flags} - < "${aur_pkgs_file}"
 }
 
 # *****************************************************************************
