c5bbf08d1c410e8a442626889f9d12330388620f
anntnzrb <anntnzrb@protonmail.com>
2023-08-23 10:06:05 -0500

    chore(nix): disable command-not-found

--- nix/hosts/shared/global/default.nix
+++ nix/hosts/shared/global/default.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 {
+  # REVIEW: this might change in the future to suppress annoying db warning
+  programs.command-not-found.enable = false;
+
   environment.systemPackages = with pkgs; [
     tree
   ];
