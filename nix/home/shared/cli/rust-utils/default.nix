c512b2940609cb01e4056e994403a63d451b9457
anntnzrb <anntnzrb@protonmail.com>
2023-08-27 14:32:03 -0500

    chore(aliases): add

--- nix/home/shared/cli/rust-utils/default.nix
+++ nix/home/shared/cli/rust-utils/default.nix
@@ -7,4 +7,21 @@
     fd
     (ripgrep.override { withPCRE2 = true; })
   ];
+
+  home.shellAliases = {
+    # ls/tree => exa
+    ls = "exa --color=automatic --group-directories-first --icons --sort=Name -Fagh";
+    ll = "exa --color=automatic --group-directories-first --icons --sort=Name -Faglh";
+
+    tree = "exa --color=automatic --group-directories-first --icons -FTgh";
+    treea = "exa --color=automatic --group-directories-first --icons -FTagh";
+    treed = "exa --color=automatic --icons -DFTgh";
+
+    # grep => rg
+    grep = "rg --color=auto --column --hidden -Hin";
+
+    # cat/less => bat
+    cat = "bat --color=auto --theme='Monokai Extended Origin' --style=full -P";
+    less = "bat --color=auto --theme='Monokai Extended Origin' --style=full";
+  };
 }
