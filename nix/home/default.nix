b0256dd30c1375c0fd5bd423bfad3249a80c3259
anntnzrb <anntnzrb@protonmail.com>
2023-08-28 12:51:51 -0500

    refactor(nix/home): simplified cfg

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -1,5 +1,25 @@
-{
-  munich = import ./munich;
-  solna = import ./solna;
-  zadar = import ./zadar;
+{ self
+, me
+, hostInfo
+, ...
+}: {
+  home.stateVersion = "23.05";
+
+  # home-manager handles itself
+  programs.home-manager.enable = true;
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  home = {
+    username = "${hostInfo.user}";
+    homeDirectory = "/home/${hostInfo.user}";
+  };
+
+  # reload services on switch
+  systemd.user.startServices = "sd-switch";
 }
