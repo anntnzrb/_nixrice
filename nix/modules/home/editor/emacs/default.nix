555f45a8bd4c9b14b9ce5955d919d837c3ea53f6
anntnzrb <anntnzrb@protonmail.com>
2023-09-06 10:47:06 -0500

    chore: cleanup

--- nix/modules/home/editor/emacs/default.nix
+++ nix/modules/home/editor/emacs/default.nix
@@ -4,22 +4,15 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkIf;
   inherit (lib.liberion) mkOptBool';
 
   cfg = config.liberion.editor.emacs;
-
-  emacsPackages = epkgs:
-    with epkgs; [
-      nix-mode
-      evil
-    ];
 in {
   options.liberion.editor.emacs = {
     enable = mkOptBool' false;
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     programs.emacs = {
       enable = true;
       package = pkgs.emacs29.override {
@@ -27,7 +20,6 @@ in {
         withGTK3 = true;
         withTreeSitter = true; # emacs29+
       };
-      extraPackages = emacsPackages;
     };
   };
 }
