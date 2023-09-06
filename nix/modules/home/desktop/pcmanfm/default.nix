6e89d77162fd81a1e48a072cab6fcc7ef9a0d947
anntnzrb <anntnzrb@protonmail.com>
2023-09-06 10:32:06 -0500

    feat(pcmanfm): use generators for ini cfg

--- nix/modules/home/desktop/pcmanfm/default.nix
+++ nix/modules/home/desktop/pcmanfm/default.nix
@@ -4,24 +4,49 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkIf;
   inherit (lib.liberion) mkOptBool';
 
   cfg = config.liberion.desktop.pcmanfm;
 in {
   options.liberion.desktop.pcmanfm = {
     enable = mkOptBool' false;
+
+    settings = lib.mkOption {
+      type = with lib.types; attrsOf anything;
+      default = {
+        config = {
+          bm_open_method = 0;
+        };
+
+        volume = {
+          mount_on_startup = 1;
+          mount_removable = 1;
+          autorun = 1;
+        };
+
+        ui = {
+          always_show_tabs = 1;
+          media_in_new_tab = 0;
+          close_on_unmount = 1;
+          side_pane_mode = "places";
+          view_mode = "icon";
+          show_hidden = 1;
+          show_statusbar = 1;
+          pathbar_mode_buttons = 0;
+        };
+      };
+      description = "Settings for PCManFM.";
+    };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     home.packages = [pkgs.pcmanfm];
 
     xdg.configFile = {
       "pcmanfm" = {
         enable = true;
-        source = ./config/pcmanfm;
-        target = "pcmanfm";
-        recursive = true;
+        target = "pcmanfm/pcmanfm.conf";
+        text = lib.generators.toINI {} cfg.settings;
       };
     };
   };
