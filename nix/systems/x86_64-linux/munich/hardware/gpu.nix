3f873036f1bfa55885eb673c20d66a3d9542c2a6
anntnzrb <anntnzrb@protonmail.com>
2024-01-23 03:55:11 -0500

    munich/gpu: drop hyprland stuff

--- nix/systems/x86_64-linux/munich/hardware/gpu.nix
+++ nix/systems/x86_64-linux/munich/hardware/gpu.nix
@@ -1,11 +1,6 @@
-{ config
-, lib
-, pkgs
+{ pkgs
 , ...
 }:
-let
-  hyprlandEnabled = config.programs.hyprland.enable;
-in
 {
   hardware.opengl = {
     enable = true;
@@ -24,16 +19,5 @@ in
     package = pkgs.linuxPackages_xanmod_latest.nvidia_x11;
 
     modesetting.enable = true;
-  };
-
-  # misc
-  programs.hyprland.enableNvidiaPatches = hyprlandEnabled;
-  environment.sessionVariables = lib.mkIf hyprlandEnabled {
-    "__GLX_VENDOR_LIBRARY_NAME" = "nvidia";
-    LIBVA_DRIVER_NAME = "nvidia";
-    XDG_SESSION_TYPE = "wayland";
-    GBM_BACKEND = "nvidia-drm";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
   };
 }
