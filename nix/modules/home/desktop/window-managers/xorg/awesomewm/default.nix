6f7c6bc26b276d2292e4923b121ed98e93149a4f
anntnzrb <anntnzrb@protonmail.com>
2024-01-31 17:32:12 -0500

    Revert "awesome: wipe"
    This reverts commit 65cc425c7fa5fb8c47c768bf296a9152e7fb9af4.

---
+++ nix/modules/home/desktop/window-managers/xorg/awesomewm/default.nix
@@ -0,0 +1,31 @@
+{ config
+, lib
+, ...
+}:
+let
+  cfg = config.liberion.desktop.window-managers.xorg.awesomewm;
+in
+{
+  options.liberion.desktop.window-managers.xorg.awesomewm = with lib.liberion; with lib.types; {
+    enable = mkOptBool';
+
+    # -------------------------------------------------------------------------
+    # xorg
+    # -------------------------------------------------------------------------
+
+    autoStart = mkOpt' (listOf str) [ ];
+
+    autorandr = {
+      enable = mkOptBool';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    liberion.common.xorg = {
+      enable = true;
+      inherit (cfg) autorandr autoStart;
+    };
+
+    xsession.windowManager.awesome.enable = true;
+  };
+}
