c0c90d2698b0dd77912941fd2b24851471654355
anntnzrb <anntnzrb@proton.me>
2024-02-19 02:01:40 -0500

    chore(flake): minor edits

--- flake.nix
+++ flake.nix
@@ -8,27 +8,27 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Flake framework
-    snowfall-lib.url = "github:snowfallorg/lib";
+    snowfall-lib.url = "github:snowfallorg/lib/main";
     snowfall-lib.inputs.nixpkgs.follows = "nixpkgs-stable";
 
     # Collection of hardware modules for systems
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     # Generate NixOS images (iso, sd, amazon, ...)
-    nixos-generators.url = "github:nix-community/nixos-generators";
+    nixos-generators.url = "github:nix-community/nixos-generators/master";
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs-stable";
 
     # User environment manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11"; # NOTE: match nixpkgs
     home-manager.inputs.nixpkgs.follows = "nixpkgs-stable";
 
     # Firefox extensions (add-ons)
-    firefox-addons.url = "github:nix-community/nur-combined?dir=repos/rycee/pkgs/firefox-addons";
+    firefox-addons.url = "github:nix-community/nur-combined/master?dir=repos/rycee/pkgs/firefox-addons";
     firefox-addons.inputs.nixpkgs.follows = "nixpkgs-stable";
 
     # Neovim
-    neovim-annt.url = "github:anntnzrb/nixvim";
-    neovim-annt.inputs.nixpkgs.follows = "nixpkgs-unstable"; # NOTE: must use nixpkgs-unstable
+    neovim-annt.url = "github:anntnzrb/nixvim/main";
+    neovim-annt.inputs.nixpkgs.follows = "nixpkgs-unstable"; # NOTE: must use unstable
   };
 
   outputs = inputs:
