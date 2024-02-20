273f0645ca5a5ec35577d567af6edb4328849b22
anntnzrb <anntnzrb@proton.me>
2024-02-20 12:36:42 -0500

    chromium: add

---
+++ nix/modules/home/desktop/browsers/chromium/default.nix
@@ -0,0 +1,30 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+let
+  cfg = config.liberion.desktop.browsers.chromium;
+in
+{
+  options.liberion.desktop.browsers.chromium = with lib.liberion; {
+    enable = mkOptBool';
+  };
+  config = lib.mkIf cfg.enable {
+    programs.chromium = {
+      enable = true;
+
+      dictionaries = [ pkgs.hunspellDictsChromium.en_US ];
+      extensions = [
+        { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
+        { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
+        { id = "eanggfilgoajaocelnaflolkadkeghjp"; } # Harpa AI
+        { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
+
+        # streaming
+        { id = "ammjkodgmmoknidbanneddgankgfejfh"; } # 7TV
+        { id = "ajopnjidmegmdimjlfnijceegpefgped"; } # BetterTTV
+      ];
+    };
+  };
+}
