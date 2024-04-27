a40cf774231e38086b3ff8f88d056c159277f11b
anntnzrb <anntnzrb@proton.me>
2024-04-26 22:35:13 -0500

    feat: add internationalization settings

--- nix/modules/nixos/default.nix
+++ nix/modules/nixos/default.nix
@@ -32,12 +32,30 @@ in
     system.stateVersion = "22.05";
 
     nix = {
-      settings.trusted-users = [ "root" "@wheel" ];
+      settings = {
+        trusted-users = [ "root" "@wheel" ];
+        experimental-features = [ "nix-command" "flakes" ];
+      };
 
       gc = {
         automatic = true;
         dates = "weekly";
         randomizedDelaySec = "45min";
+      };
+    };
+
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+      extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
       };
     };
 
