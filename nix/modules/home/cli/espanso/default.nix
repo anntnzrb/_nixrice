30ba29d339c773f697d788edb1d1bdc3bbd42c96
anntnzrb <anntnzrb@proton.me>
2024-05-17 16:32:21 -0500

    feat: add espanso module

---
+++ nix/modules/home/cli/espanso/default.nix
@@ -0,0 +1,18 @@
+{ config
+, lib
+, ...
+}:
+let
+  cfg = config.liberion.cli.espanso;
+in
+{
+  options.liberion.cli.espanso = with lib.liberion; {
+    enable = mkOptBool';
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.espanso = {
+      enable = true;
+    };
+  };
+}
