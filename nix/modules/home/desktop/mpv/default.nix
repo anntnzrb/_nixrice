037ce320895ecbbb7dde9ec2d32124b78b36e98b
anntnzrb <anntnzrb@proton.me>
2024-06-11 20:49:18 -0500

    mpv: bring back binds

--- nix/modules/home/desktop/mpv/default.nix
+++ nix/modules/home/desktop/mpv/default.nix
@@ -24,8 +24,41 @@ in
         border = false;
       };
 
-      # disabled because of uosc script
-      #bindings = import ./bindings.nix;
+      bindings =
+        let
+          # Seek {for,back}-ward by a number of seconds
+          # seek: number -> string
+          seek = val: "no-osd seek ${toString val} exact";
+
+          # {In,De}-crease volume by a percentage value
+          # speed: number -> string
+          speed = val: "add speed ${toString val}";
+
+          # {In,De}-crease volume by a percentage value
+          # vol: number -> string
+          vol = val: "add volume ${toString val}";
+
+          # Disable a keybinding
+          # disabled -> string
+          disabled = "noop";
+        in
+        {
+          "LEFT" = seek (-5);
+          "RIGHT" = seek 5;
+          "Ctrl+LEFT" = seek (-60);
+          "Ctrl+RIGHT" = seek 60;
+
+          "[" = disabled;
+          "]" = disabled;
+          "<" = speed (-0.05);
+          ">" = speed 0.05;
+
+          "DOWN" = vol (-2);
+          "UP" = vol 2;
+          "m" = "cycle mute";
+
+          "q" = "quit-watch-later";
+        };
     };
   };
 }
