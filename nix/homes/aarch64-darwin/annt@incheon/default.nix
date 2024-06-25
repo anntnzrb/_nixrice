6e42c3e2a7c1ac60ee49e8404f4083e5b2d1b307
anntnzrb <anntnzrb@proton.me>
2024-06-25 09:57:59 -0500

    nix/systems: init incheon

---
+++ nix/homes/aarch64-darwin/annt@incheon/default.nix
@@ -0,0 +1,39 @@
+{ pkgs
+, lib
+, ...
+}:
+
+with lib.liberion; {
+  liberion = {
+    shells = {
+      defaults = on;
+      altCoreUtils = on;
+      bash = on;
+    };
+
+    cli = {
+      btop = on;
+      direnv = on;
+      fastfetch = on;
+      fzf = on;
+      git = on;
+      starship = on;
+      tldr = on;
+      yt-dlp = on;
+      zoxide = on;
+    };
+
+    editors = {
+      neovim = on;
+      vscode = on;
+    };
+
+    desktop = {
+      browsers = {
+        firefox = on;
+        chromium = on;
+      };
+      terminal-emulators.alacritty = on;
+    };
+  };
+}
