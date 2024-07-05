89ca616f0a6cf5e9b0210fe1726d840686c6cbc3
anntnzrb <anntnzrb@proton.me>
2024-07-05 14:27:50 -0500

    darwin: add basic yabai support

---
+++ nix/modules/darwin/services/yabai/default.nix
@@ -0,0 +1,104 @@
+{ config
+, lib
+, namespace
+, ...
+}:
+let
+  cfg = config.${namespace}.darwin.services.yabai;
+
+  # helpers
+  formatAttrs = with builtins; attrs: concatStringsSep " " (map (k: "${k}=${getAttr k attrs}") (attrNames attrs));
+  mkRule = rule: "yabai -m rule --add ${formatAttrs rule.pattern} ${formatAttrs rule.ruleset}";
+
+in
+{
+  options.${namespace}.darwin.services.yabai = with lib.${namespace}; {
+    enable = mkOptBool';
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.yabai = {
+      enable = true;
+
+      config = {
+        layout = "bsp";
+        split_ratio = 0.5;
+        focus_follows_mouse = "autofocus";
+
+        # mouse
+        mouse_modifier = "fn";
+        mouse_follows_focus = "on"; # warp mouse?
+        mouse_action1 = "move"; # mod + LMB
+        mouse_action2 = "resize"; # mod + RMB
+
+        # gaps
+        top_padding = 10;
+        bottom_padding = 10;
+        left_padding = 15;
+        right_padding = 15;
+        window_gap = 15;
+      };
+
+      extraConfig =
+        let
+          rules = [
+            # sys
+            {
+              pattern = { app = "^System Settings$"; };
+              ruleset = { manage = "off"; };
+            }
+            {
+              pattern = { app = "^System Information$"; };
+              ruleset = { manage = "off"; };
+            }
+            {
+              pattern = { app = "^System Preferences$"; };
+              ruleset = { manage = "off"; };
+            }
+            {
+              pattern = { title = "Preferences$"; };
+              ruleset = { manage = "off"; };
+            }
+            {
+              pattern = { title = "Settings$"; };
+              ruleset = { manage = "off"; };
+            }
+            {
+              pattern = { app = "^Finder$"; };
+              ruleset = { manage = "off"; };
+            }
+
+            # misc
+            {
+              pattern = { app = "^Terminal$"; };
+              ruleset = { manage = "off"; };
+            }
+            {
+              pattern = { app = "^Calculator$"; };
+              ruleset = { manage = "off"; };
+            }
+            {
+              pattern = { app = "^Notes$"; };
+              ruleset = { manage = "off"; };
+            }
+            {
+              pattern = { app = "^Alacritty$"; };
+              ruleset = { manage = "off"; };
+            }
+            {
+              pattern = { app = "^ChatGPT$"; };
+              ruleset = { manage = "off"; };
+            }
+            {
+              pattern = { app = "^WhatsApp$"; };
+              ruleset = { manage = "off"; };
+            }
+          ];
+        in
+        ''
+          # rules
+          ${builtins.concatStringsSep "\n" (map mkRule rules)}
+        '';
+    };
+  };
+}
