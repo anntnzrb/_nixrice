32d4ab4067b8f2f79030f3db8cf875dd969dc465
anntnzrb <anntnzrb@proton.me>
2024-07-05 14:27:50 -0500

    darwin: add basic yabai support

--- nix/systems/aarch64-darwin/incheon/default.nix
+++ nix/systems/aarch64-darwin/incheon/default.nix
@@ -2,10 +2,18 @@
 , namespace
 , ...
 }: {
-  ${namespace} = with lib.liberion; {
+  # zsh as an interactive shell; this is a force default
+  # customization is done via hm
+  programs.zsh.enable = true;
+
+  ${namespace} = with lib.${namespace}; {
     darwin = {
       system = {
         trackpad = on;
+      };
+
+      services = {
+        yabai = on;
       };
 
       homebrew = on;
