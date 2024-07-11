49b2dbfbae1b9e3a2c4ce738a01b085a83763cd7
anntnzrb <anntnzrb@proton.me>
2024-07-11 17:18:14 -0500

    lib: add new alias

--- nix/lib/module/default.nix
+++ nix/lib/module/default.nix
@@ -2,25 +2,31 @@
 , ...
 }: rec {
 
-  # Alias for mkOption.
+  # Alias for mkOption
   mkOpt = type: default: description:
     lib.mkOption { inherit type default description; };
 
-  # Alias for mkOption with no description.
-  mkOpt' = type: default: mkOpt type default null;
+  # Alias for mkOption with no description
+  mkOpt' = type: def: mkOpt type def null;
 
-  # Alias for mkEnableOption.
+  # Alias for mkEnableOption
   mkOptBool = desc: lib.mkEnableOption desc;
 
-  # Alias for mkEnableOption with no description.
+  # Alias for mkEnableOption with no description
   mkOptBool' = mkOptBool null;
 
-  # Alias for enabling a service.
+  # Alias for mkEnableOption but defaulting to true
+  mkOptEnabled = desc: mkOpt lib.types.bool true desc;
+
+  # Alias for mkEnableOption but defaulting to true with no description
+  mkOptEnabled' = mkOpt lib.types.bool true null;
+
+  # Alias for enabling a service
   on = {
     enable = true;
   };
 
-  # Alias for disabling a service.
+  # Alias for disabling a service
   off = {
     enable = false;
   };
