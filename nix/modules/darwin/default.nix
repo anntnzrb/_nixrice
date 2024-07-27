d337cb82a63a796ab168e6bcad88e0093ca77f19
anntnzrb <anntnzrb@proton.me>
2024-07-27 13:20:07 -0500

    flake: add nix-community binary cache

--- nix/modules/darwin/default.nix
+++ nix/modules/darwin/default.nix
@@ -18,6 +18,14 @@ in
     nix.settings = {
       auto-optimise-store = true;
       trusted-users = [ "root" "@admin" ];
+
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     services.nix-daemon.enable = true;
 
--- nix/modules/nixos/default.nix
+++ nix/modules/nixos/default.nix
@@ -35,6 +35,14 @@ in
       settings = {
         trusted-users = [ "root" "@wheel" ];
         experimental-features = [ "nix-command" "flakes" ];
+
+        substituters = [
+          "https://nix-community.cachix.org"
+        ];
+
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
       };
 
       gc = {
