2016fa106c88815018d840f278fd54641dfb50ac
anntnzrb <anntnzrb@proton.me>
2024-08-25 16:54:27 -0500

    zadar: cleanup

--- nix/homes/x86_64-linux/annt@zadar/default.nix
+++ nix/homes/x86_64-linux/annt@zadar/default.nix
@@ -3,24 +3,14 @@
 , ...
 }:
 
-with lib.${namespace}; let
-  autoStart = [
-    "nm-applet"
-    "pasystray"
-    "\${TERMINAL} -e btop"
-  ];
-in
-{
+with lib.${namespace}; {
   liberion = {
     shells = {
       defaults = on;
       altCoreUtils = on;
 
       sessionVariables = {
-        BROWSER = "chromium";
         EDITOR = "nvim";
-        FILE = "pcmanfm";
-        TERMINAL = "alacritty";
       };
 
       bash = {
@@ -40,7 +30,6 @@ in
       direnv = on;
       fastfetch = on;
       fzf = on;
-      notesnook = on;
       simple-mtpfs = on;
       tldr = on;
       yt-dlp = on;
@@ -50,49 +39,5 @@ in
     editors = {
       neovim = on;
     };
-
-    desktop = {
-      sxhkd = {
-        enable = true;
-        timeout = 3;
-        cancelKey = "Escape";
-      };
-
-      launchers.bemenu = on;
-
-      window-managers = {
-        xorg = {
-          awesomewm = {
-            enable = true;
-            compositor.picom.enable = true;
-            inherit autoStart;
-          };
-        };
-      };
-
-      browsers = {
-        chromium = on;
-        qutebrowser = on;
-      };
-      feh = on;
-      file-managers.pcmanfm = on;
-      flameshot = on;
-      mpv = on;
-      gammastep = on;
-      terminal-emulators.alacritty = on;
-      zathura = on;
-
-      ui = {
-        themes = on;
-        fonts = on;
-      };
-    };
-  };
-
-  services.cbatticon = {
-    enable = true;
-    updateIntervalSeconds = 30;
-    lowLevelPercent = 25;
-    criticalLevelPercent = 10;
   };
 }
