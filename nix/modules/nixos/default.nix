38c0d3ef53474b59306fa7e622b86ae3cd83bba9
anntnzrb <anntnzrb@proton.me>
2024-09-12 11:49:52 -0500

    feat: add self cache (cachix)

--- nix/modules/nixos/default.nix
+++ nix/modules/nixos/default.nix
@@ -39,11 +39,13 @@ in
         substituters = [
           "https://nix-community.cachix.org"
           "https://devenv.cachix.org"
+          "anntnzrb.cachix.org"
         ];
 
         trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          "anntnzrb.cachix.org-1:hG29RyjX45a9q1nZqdvOJUQ6nRDG/Jj4yt2d1dpWCgE="
         ];
       };
 
