961574ab7d410f8a02ac73bbcb42690b459ed532
anntnzrb <anntnzrb@proton.me>
2024-09-17 20:27:54 -0500

    feat: add pre-commit-hooks checks

---
+++ nix/checks/pre-commit-hooks/default.nix
@@ -0,0 +1,35 @@
+{ inputs
+, pkgs
+, ...
+}:
+inputs.pre-commit-hooks.lib.${pkgs.system}.run {
+  src = inputs.self;
+
+  hooks = {
+    actionlint.enable = true;
+    deadnix = {
+      enable = true;
+      settings = {
+        edit = true;
+        noUnderscore = true;
+      };
+    };
+
+    statix =
+      let
+        cfg = (pkgs.formats.toml { }).generate "statix.toml" { disabled = disabled-lints; };
+        disabled-lints = [ "repeated_keys" ];
+      in
+      {
+        enable = true;
+        package = pkgs.writeShellApplication {
+          name = "statix";
+          runtimeInputs = [ pkgs.statix ];
+          text = ''
+            shift
+            exec statix check --config ${cfg} "$@"
+          '';
+        };
+      };
+  };
+}
