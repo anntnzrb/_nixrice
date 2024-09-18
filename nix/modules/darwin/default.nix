76462dfb363f0f106080dfd41ab65fbf18828d6e
anntnzrb <anntnzrb@proton.me>
2024-09-17 20:28:05 -0500

    refactor: apply checks (deadnix)

--- nix/modules/darwin/default.nix
+++ nix/modules/darwin/default.nix
@@ -64,7 +64,7 @@ in
       activationScripts = {
         xcodeInstall.text = "xcode-select --print-path >/dev/null 2>&1 || xcode-select --install >/dev/null 2>&1";
         rosettaInstall = {
-          enable = (system == "aarch64-darwin");
+          enable = system == "aarch64-darwin";
           text = "pgrep oahd >/dev/null 2>&1 || softwareupdate --install-rosetta --agree-to-license >/dev/null 2>&1";
         };
       };
