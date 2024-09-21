df2f94317e8944567ca10223ae54360a7649f027
anntnzrb <anntnzrb@proton.me>
2024-09-21 18:55:37 -0500

    refactor: idiomatic tweaks

--- nix/modules/home/desktop/window-managers/xorg/herbstluftwm/default.nix
+++ nix/modules/home/desktop/window-managers/xorg/herbstluftwm/default.nix
@@ -1,29 +1,27 @@
 {
-  config,
   lib,
   pkgs,
+  config,
+  namespace,
   ...
 }:
 let
-  cfg = config.liberion.desktop.window-managers.xorg.herbstluftwm;
+  cfg = config.${namespace}.desktop.window-managers.xorg.herbstluftwm;
 in
 {
-  options.liberion.desktop.window-managers.xorg.herbstluftwm =
-    with lib.liberion;
-    with lib.types;
-    {
-      enable = mkOptBool';
+  options.${namespace}.desktop.window-managers.xorg.herbstluftwm = with lib.${namespace}; {
+    enable = mkOptBool';
 
-      compositor = {
-        picom.enable = mkOptBool';
-      };
-
-      autoStart = mkOpt' (listOf str) [ ];
-      keys.super = mkOpt' str "Mod4";
+    compositor = {
+      picom.enable = mkOptBool';
     };
 
+    autoStart = with lib.types; mkOpt' (listOf str) [ ];
+    keys.super = mkOpt' str "Mod4";
+  };
+
   config = lib.mkIf cfg.enable {
-    liberion.common.xorg = {
+    ${namespace}.common.xorg = {
       enable = true;
       inherit (cfg.compositor) picom;
     };
@@ -41,39 +39,41 @@ in
         frame_padding = -12;
       };
 
-      keybinds = lib.mkMerge [
-        {
-          "${cfg.keys.super}-j" = "focus down";
-          "${cfg.keys.super}-k" = "focus up";
-          "${cfg.keys.super}-h" = "focus left";
-          "${cfg.keys.super}-l" = "focus right";
-          "${cfg.keys.super}-Shift-q" = "close";
-        }
-        (builtins.listToAttrs (
-          builtins.map (
-            i:
-            let
-              tag = builtins.elemAt tags (i - 1);
-            in
-            {
-              name = "${cfg.keys.super}-${toString i}";
-              value = "use ${tag}";
-            }
-          ) (lib.lists.range 1 (builtins.length tags))
-        ))
-        (builtins.listToAttrs (
-          builtins.map (
-            i:
-            let
-              tag = builtins.elemAt tags (i - 1);
-            in
-            {
-              name = "${cfg.keys.super}-Shift-${toString i}";
-              value = "move ${tag}";
-            }
-          ) (lib.lists.range 1 (builtins.length tags))
-        ))
-      ];
+      keybinds =
+        with builtins;
+        lib.mkMerge [
+          {
+            "${cfg.keys.super}-j" = "focus down";
+            "${cfg.keys.super}-k" = "focus up";
+            "${cfg.keys.super}-h" = "focus left";
+            "${cfg.keys.super}-l" = "focus right";
+            "${cfg.keys.super}-Shift-q" = "close";
+          }
+          (listToAttrs (
+            map (
+              i:
+              let
+                tag = elemAt tags (i - 1);
+              in
+              {
+                name = "${cfg.keys.super}-${toString i}";
+                value = "use ${tag}";
+              }
+            ) (lib.lists.range 1 (length tags))
+          ))
+          (listToAttrs (
+            map (
+              i:
+              let
+                tag = elemAt tags (i - 1);
+              in
+              {
+                name = "${cfg.keys.super}-Shift-${toString i}";
+                value = "move ${tag}";
+              }
+            ) (lib.lists.range 1 (length tags))
+          ))
+        ];
 
       mousebinds = {
         "${cfg.keys.super}-B1" = "move";
--- nix/modules/home/shells/default.nix
+++ nix/modules/home/shells/default.nix
@@ -6,17 +6,19 @@
   ...
 }:
 let
+  inherit (lib) getExe;
+
   cfg = config.${namespace}.shells;
 
   cmd = rec {
     eza = {
       flags = "--color=auto --group-directories-first --icons";
-      bin = "${lib.getExe pkgs.eza} ${eza.flags}";
+      bin = "${getExe pkgs.eza} ${eza.flags}";
     };
 
     bat = {
       flags = "--color=auto --theme='Monokai Extended Origin' --style=full";
-      bin = "${lib.getExe pkgs.bat} ${bat.flags}";
+      bin = "${getExe pkgs.bat} ${bat.flags}";
     };
   };
 in
@@ -27,53 +29,44 @@ in
   };
 
   config = {
-    home.sessionVariables = lib.mkMerge [
-      { NIX_SHELL_PRESERVE_PROMPT = "1"; }
-      cfg.sessionVariables
-    ];
+    home.sessionVariables = {
+      NIX_SHELL_PRESERVE_PROMPT = "1";
+    } // (lib.optionals (cfg.sessionVariables != null) cfg.sessionVariables);
 
     home.shellAliases =
       with pkgs;
-      let
-        inherit (lib) mkIf getExe;
+      (lib.optionals cfg.aliases.defaults.enable {
+        ".." = "cd ..";
+        cp = "cp -Riv";
+        diff = "diff --color=auto";
+        mkdir = "mkdir -pv";
+        lsblk = "lsblk -ai";
+        mv = "mv -iv";
+        rm = "rm -v";
+        rmfr = "rm -Rfv";
+        wget = "${getExe wget} --no-hsts";
+        zip = "${getExe zip} -rv";
 
-        defaults = mkIf cfg.aliases.defaults.enable {
-          ".." = "cd ..";
-          cp = "cp -Riv";
-          diff = "diff --color=auto";
-          mkdir = "mkdir -pv";
-          lsblk = "lsblk -ai";
-          mv = "mv -iv";
-          rm = "rm -v";
-          rmfr = "rm -Rfv";
-          wget = "${getExe wget} --no-hsts";
-          zip = "${getExe zip} -rv";
+        # nix
+        nix-lockfile-update = "nix flake update --commit-lock-file --option commit-lockfile-summary 'chore(flake): update lockfile'";
+        nix-man = "${getExe man} configuration.nix";
+        nix-man-hm = "${getExe man} home-configuration.nix";
 
-          # nix
-          nix-lockfile-update = "nix flake update --commit-lock-file --option commit-lockfile-summary 'chore(flake): update lockfile'";
-          nix-man = "${getExe man} configuration.nix";
-          nix-man-hm = "${getExe man} home-configuration.nix";
+        # ls/tree => eza
+        ls = "${cmd.eza.bin} --sort=Name -agh";
+        ll = "${cmd.eza.bin} --sort=Name -aglh";
 
-          # ls/tree => eza
-          ls = "${cmd.eza.bin} --sort=Name -agh";
-          ll = "${cmd.eza.bin} --sort=Name -aglh";
+        tree = "${cmd.eza.bin} -Tgh";
+        treea = "${cmd.eza.bin} -Tagh";
+        treed = "${cmd.eza.bin} -DTgh";
 
-          tree = "${cmd.eza.bin} -Tgh";
-          treea = "${cmd.eza.bin} -Tagh";
-          treed = "${cmd.eza.bin} -DTgh";
+        # grep => rg (ripgrep)
+        grep = "${getExe ripgrep} --color=auto --column --hidden -Hin";
 
-          # grep => rg (ripgrep)
-          grep = "${getExe ripgrep} --color=auto --column --hidden -Hin";
-
-          # cat/less => bat
-          cat = "${cmd.bat.bin} -P";
-          less = "${cmd.bat.bin}";
-        };
-      in
-      lib.mkMerge [
-        { }
-        defaults
-      ];
+        # cat/less => bat
+        cat = "${cmd.bat.bin} -P";
+        less = "${cmd.bat.bin}";
+      });
 
     home.packages = with pkgs; [
       bat
