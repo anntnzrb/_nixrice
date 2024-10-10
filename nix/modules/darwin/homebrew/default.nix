f2c591f258bb29f20858c836ecded5c87470fee8
anntnzrb <anntnzrb@proton.me>
2024-10-10 02:05:53 -0500

    darwin(brew): no analytics

--- nix/modules/darwin/homebrew/default.nix
+++ nix/modules/darwin/homebrew/default.nix
@@ -45,8 +45,9 @@ in
       };
 
     environment.variables = {
-      HOMEBREW_NO_INSECURE_REDIRECT = "1";
+      HOMEBREW_NO_ANALYTICS = "1";
       HOMEBREW_NO_EMOJI = "1";
+      HOMEBREW_NO_INSECURE_REDIRECT = "1";
 
       # handled by nix; unneeded
       HOMEBREW_NO_ENV_HINTS = "0";
