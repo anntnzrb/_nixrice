85c815b36a558fe18ed360fa86125648773aac1d
anntnzrb <anntnzrb@proton.me>
2024-10-21 15:32:38 -0500

    darwin: add finder tweaks

--- nix/modules/darwin/system/finder/default.nix
+++ nix/modules/darwin/system/finder/default.nix
@@ -1,18 +1,46 @@
 { config, namespace, ... }:
 let
   _cfg = config.${namespace}.system.finder;
+
+  searchScope = {
+    thisMac = null;
+    currentFolder = "SCcf";
+  };
+
+  viewStyle = {
+    icon = "icnv";
+    list = "clmv";
+    galery = "Flwv";
+  };
 in
 {
   config = {
-    system.defaults.finder = {
-      AppleShowAllExtensions = true;
-      AppleShowAllFiles = true;
-      CreateDesktop = false;
-      FXEnableExtensionChangeWarning = false;
-      FXPreferredViewStyle = "Nlsv";
-      QuitMenuItem = true;
-      ShowPathbar = true;
-      ShowStatusBar = true;
+    system.defaults = {
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        CreateDesktop = false;
+        FXDefaultSearchScope = searchScope.currentFolder;
+        FXEnableExtensionChangeWarning = false;
+        FXPreferredViewStyle = viewStyle.list;
+        QuitMenuItem = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+        _FXSortFoldersFirst = true;
+      };
+
+      CustomUserPreferences."com.apple.finder" = {
+        ShowExternalHardDrivesOnDesktop = true;
+        ShowHardDrivesOnDesktop = true;
+        ShowMountedServersOnDesktop = true;
+        ShowRemovableMediaOnDesktop = true;
+
+        # finder startup is ~/
+        NewWindowTargetPath = "file:///Users/${config.${namespace}.darwin.user.name}/";
+        NewWindowTarget = "PfHm";
+        # multi-file tab view
+        FinderSpawnTab = true;
+      };
     };
   };
 }
