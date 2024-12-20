86085861d98e8061a3a3f2209ee914a961094d64
anntnzrb <anntnzrb@proton.me>
2024-12-19 21:23:37 -0500

    feat(zed): use unstable module

--- nix/modules/home/editors/zed/default.nix
+++ nix/modules/home/editors/zed/default.nix
@@ -2,18 +2,27 @@
   lib,
   pkgs,
   config,
+  inputs,
   namespace,
   ...
 }:
 let
   cfg = config.${namespace}.editors.zed;
+  mod = "programs/zed-editor.nix";
 in
 {
+  disabledModules = [ mod ];
+  imports = [ (import (inputs.home-manager-unstable + "/modules/${mod}")) ];
+
   options.${namespace}.editors.zed = with lib.${namespace}; {
     enable = mkOptBool';
   };
 
-  config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs.zed-editor ];
+  config.programs.zed-editor = lib.mkIf cfg.enable {
+    enable = true;
+    extraPackages = with pkgs; [
+      nixd
+      nixfmt-rfc-style
+    ];
   };
 }
