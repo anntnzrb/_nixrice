db553d3e9fb54a08b1fbe3832355e940e355f4ed
anntnzrb <anntnzrb@proton.me>
2024-12-20 22:34:28 -0500

    shell: add pre-commit shellHook

--- nix/shells/default/default.nix
+++ nix/shells/default/default.nix
@@ -1,6 +1,13 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  inputs,
+  system,
+  ...
+}:
 pkgs.mkShellNoCC {
   name = "liberion-shell";
+
+  inherit (inputs.self.checks.${system}.pre-commit-hooks) shellHook;
 
   nativeBuildInputs = with pkgs; [ just ];
 }
