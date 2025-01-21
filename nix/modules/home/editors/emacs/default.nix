a8650669a2ea9d9cb4ee8de1197c750b30fb22e2
anntnzrb <anntnzrb@proton.me>
2025-01-21 02:27:02 -0500

    feat(emacs): update module

--- nix/modules/home/editors/emacs/default.nix
+++ nix/modules/home/editors/emacs/default.nix
@@ -1,29 +1,62 @@
 {
   lib,
   pkgs,
+  inputs,
   config,
+  system,
   namespace,
   ...
 }:
 let
+  inherit (inputs.nixpkgs.legacyPackages.${system}) emacs30;
+
   cfg = config.${namespace}.editors.emacs;
 
   mkEmacsPackage =
     pkg:
     (pkgs.emacsPackagesFor pkg).emacsWithPackages (
       epkgs: with epkgs; [
+        pkgs.coreutils-prefixed # provides gls
+
+        # nix
+        pkgs.nixd # LSP
+        pkgs.nixfmt-rfc-style # fmt
+
+        # binds
+        evil
+        evil-collection
+
         treesit-grammars.with-all-grammars
 
-        evil
-        vertico
-        marginalia
-        corfu
-        fontaine
-        envrc
+        # themes
         ef-themes
 
+        # font
+        fontaine
+
+        pulsar
+
+        corfu
+        envrc
+        consult
+        orderless
+
+        # minibuffer
+        vertico
+        marginalia
+        embark
+        embark-consult
+
+        # AI
+        gptel
+
+        # fmt
+        apheleia
+
         # langs
+        nix-ts-mode
         gleam-ts-mode
+        markdown-mode
       ]
     );
 in
@@ -33,14 +66,29 @@ in
 
     package = lib.mkOption {
       type = lib.types.package;
-      default =
-        if pkgs.stdenv.isDarwin then (mkEmacsPackage pkgs.emacs29) else (mkEmacsPackage pkgs.emacs29-pgtk);
+      default = mkEmacsPackage (
+        emacs30.override {
+          withNativeCompilation = true;
+          withTreeSitter = true;
+        }
+      );
       description = "The Emacs package to install.";
     };
-
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ cfg.package ];
+    home.packages =
+      [ cfg.package ]
+      ++ [
+        # fonts
+        pkgs.iosevka-comfy.comfy-motion
+        pkgs.iosevka-comfy.comfy-wide-motion-duo
+        (pkgs.nerdfonts.override { fonts = [ "ZedMono" ]; })
+      ];
+
+    home.shellAliases = {
+      # NOTE: 'disown' is not POSIX
+      eee = "nohup ${cfg.package}/bin/emacs >/tmp/emacs-nohup.out 2>&1 & disown";
+    };
   };
 }
