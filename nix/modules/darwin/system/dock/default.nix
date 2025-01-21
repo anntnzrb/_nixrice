8b7eb3d2c6aa781398e0c8cf217f22bc20073cfc
anntnzrb <anntnzrb@proton.me>
2025-01-20 22:08:32 -0500

    darwin: dock/mission-control tweaks (gestures)

--- nix/modules/darwin/system/dock/default.nix
+++ nix/modules/darwin/system/dock/default.nix
@@ -1,11 +1,16 @@
-{ config, namespace, ... }:
+{
+  config,
+  namespace,
+  ...
+}:
 let
   _cfg = config.${namespace}.system.dock;
 in
 {
-  config = {
-    system.defaults.dock = {
+  config.system.defaults = {
+    dock = {
       autohide = true;
+      expose-group-apps = true; # shows all apps instead of per-space
       largesize = 88;
       magnification = true;
       mineffect = "scale";
@@ -15,6 +20,20 @@ in
       showhidden = false; # hide the hidden lol
       static-only = false; # allow unopened apps to be shown
       tilesize = 40;
+    };
+
+    CustomUserPreferences.NSGlobalDomain."com.apple.dock" = {
+      ## gestures
+      showAppExposeGestureEnabled = 0;
+
+      # 3f up for mission-control
+      showMissionControlGestureEnabled = 1;
+
+      # 3f shows launchpad
+      showLaunchpadGestureEnabled = 0;
+
+      # 3f shows desktop
+      showDesktopGestureEnabled = 0;
     };
   };
 }
