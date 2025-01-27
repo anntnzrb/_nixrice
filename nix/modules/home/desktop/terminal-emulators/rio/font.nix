14b474b60517bca7a6f943ba5828a0934b0686f7
anntnzrb <anntnzrb@proton.me>
2025-01-26 20:37:00 -0500

    rio: update font module

--- nix/modules/home/desktop/terminal-emulators/rio/font.nix
+++ nix/modules/home/desktop/terminal-emulators/rio/font.nix
@@ -1,29 +1,52 @@
-_: {
-  config.programs.rio.settings.fonts = {
-    size = 14;
-
-    regular = {
-      family = "Iosevka Comfy Motion";
-      style = "Normal";
-      weight = 400;
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  fonts = {
+    iosevka-comfy-motion = {
+      name = "Iosevka Comfy Motion";
+      pkg = pkgs.iosevka-comfy.comfy-motion;
     };
-
-    bold = {
-      family = "Iosevka Comfy Motion";
-      style = "Normal";
-      weight = 800;
+    zedmono-nerd = {
+      name = "ZedMono Nerd Font";
+      pkg = pkgs.nerdfonts.override {
+        fonts = [ "ZedMono" ];
+      };
     };
+  };
+in
+{
+  config = {
+    home.packages = lib.attrsets.mapAttrsToList (_: font: font.pkg) fonts;
 
-    italic = {
-      family = "Iosevka Comfy Motion";
-      style = "Italic";
-      weight = 400;
-    };
+    programs.rio.settings.fonts = {
+      size = 14;
 
-    bold-italic = {
-      family = "Iosevka Comfy Motion";
-      style = "Italic";
-      weight = 800;
+      regular = {
+        family = fonts.iosevka-comfy-motion.name;
+        style = "Normal";
+        weight = 400;
+      };
+
+      bold = {
+        family = fonts.iosevka-comfy-motion.name;
+        style = "Normal";
+        weight = 800;
+      };
+
+      italic = {
+        family = fonts.iosevka-comfy-motion.name;
+        style = "Italic";
+        weight = 400;
+      };
+
+      bold-italic = {
+        family = fonts.iosevka-comfy-motion.name;
+        style = "Italic";
+        weight = 800;
+      };
     };
   };
 }
