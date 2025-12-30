2fc379c5801115da47cc25b370aaa3ff4ff8d910
anntnzrb <anntnzrb@proton.me>
2025-12-30 18:33:15 -0500

    betterfox: update module

--- nix/modules/home/desktop/browsers/firefox/betterfox.nix
+++ nix/modules/home/desktop/browsers/firefox/betterfox.nix
@@ -18,7 +18,20 @@ in
       betterfox = on // {
         profiles.default = {
           enableAllSections = true;
+
+          settings = {
+            # snappy scrolling (60Hz)
+            smoothfox.sharpen-scrolling.enable = true;
+          };
         };
+      };
+
+      profiles.default.settings = {
+        # clear cache on close
+        "privacy.sanitize.sanitizeOnShutdown" = true;
+        "privacy.clearOnShutdown_v2.cache" = true;
+        "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+        "privacy.clearOnShutdown_v2.historyFormDataAndDownloads" = false;
       };
     };
   };
