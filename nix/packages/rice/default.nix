2fcf99f7aea37799ed71635f736f6a4eda9fe7f5
anntnzrb <anntnzrb@proton.me>
2026-02-21 19:25:21 -0500

    refactor(rice): derive package metadata from Cargo.toml

--- nix/packages/rice/default.nix
+++ nix/packages/rice/default.nix
@@ -9,11 +9,37 @@
 */
 {
   pkgs,
+  inputs,
   ...
 }:
-pkgs.rustPlatform.buildRustPackage {
-  pname = "rice";
-  version = "0.1.0";
+let
+  cargoToml = builtins.fromTOML (builtins.readFile ./src/Cargo.toml);
+  inherit (cargoToml) package;
+  pname = package.name;
+  inherit (package) version;
+
+  toolchain =
+    inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.complete.withComponents
+      [
+        "cargo"
+        "rustc"
+        "clippy"
+        "rustfmt"
+      ];
+
+  rustPlatform = pkgs.makeRustPlatform {
+    cargo = toolchain;
+    rustc = toolchain;
+  };
+in
+rustPlatform.buildRustPackage {
+  inherit pname version;
   src = ./src;
   cargoLock.lockFile = ./src/Cargo.lock;
+
+  postCheck = ''
+    cargo fmt --all --check
+    cargo clippy --all-targets --all-features --offline --frozen -- -D warnings
+    cargo doc --no-deps --offline --frozen
+  '';
 }
