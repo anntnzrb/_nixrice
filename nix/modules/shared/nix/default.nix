4fe90b7d227f16af326b5cbad6a926547e2f9255
anntnzrb <anntnzrb@proton.me>
2026-05-24 21:40:45 -0500

    chore(nix): updated numtide binary cache public key
    - Switched binary cache host from cache.numtide.com to niks3.numtide.com.

--- nix/modules/shared/nix/default.nix
+++ nix/modules/shared/nix/default.nix
@@ -52,7 +52,7 @@ let
     "https://nix-community.cachix.org" =
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
     "https://cache.numtide.com" =
-      "cache.numtide.com-1:GF3TabtFocLtonIGfz3PD61AgIO8GmjCYhEAmYy4VPY=";
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=";
     "https://devenv.cachix.org" =
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     "https://anntnzrb.cachix.org" =
