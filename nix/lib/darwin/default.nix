185ba0a525a056be5ee47fc15e5332f89d22612c
anntnzrb <anntnzrb@proton.me>
2026-05-30 21:07:19 -0500

    feat(nix/lib): Added darwin program helpers for cask and mas apps
    - created mkOneCaskProgram and mkOneMasAppProgram to reduce boilerplate in darwin program modules

---
+++ nix/lib/darwin/default.nix
@@ -0,0 +1,5 @@
+{ lib, ... }:
+
+{
+  darwin.programs = import ./programs { inherit lib; };
+}
---
+++ nix/lib/darwin/programs/default.nix
@@ -0,0 +1,51 @@
+{ lib, ... }:
+let
+  inherit (lib.types)
+    ints
+    str
+    ;
+in
+{
+  mkOneCaskProgram =
+    { config, namespace }:
+    name: cask:
+    let
+      inherit (lib.${namespace}.module)
+        mkOptDisabled'
+        ;
+      cfg = config.${namespace}.programs.${name};
+    in
+    {
+      options = lib.setAttrByPath [ namespace "programs" name ] {
+        enable = mkOptDisabled';
+      };
+
+      config = lib.mkIf cfg.enable {
+        ${namespace}.homebrew.packages.casks = [ cask ];
+      };
+    };
+
+  mkOneMasAppProgram =
+    { config, namespace }:
+    name: appName: appId:
+    let
+      inherit (lib.${namespace}.module)
+        mkOpt'
+        mkOptDisabled'
+        ;
+      cfg = config.${namespace}.programs.${name};
+    in
+    {
+      options = lib.setAttrByPath [ namespace "programs" name ] {
+        enable = mkOptDisabled';
+        masAppName = mkOpt' str appName;
+        masAppId = mkOpt' ints.positive appId;
+      };
+
+      config = lib.mkIf cfg.enable {
+        ${namespace}.homebrew.packages.masApps = {
+          "${cfg.masAppName}" = cfg.masAppId;
+        };
+      };
+    };
+}
