9073bb92be9076ca250170bf9e26b6fc81b988c8
anntnzrb <anntnzrb@proton.me>
2026-07-18 20:16:04 -0500

    Revert "feat(nix/aerospace): Enabled focus-follows-mouse behavior"
    This reverts commit e5446647c9ef44111f5599ab2eba543acfd2f421.

--- nix/modules/darwin/services/aerospace/nodes.nix
+++ nix/modules/darwin/services/aerospace/nodes.nix
@@ -12,9 +12,6 @@ in
   config = lib.mkIf cfg.enable {
     services.aerospace.settings = {
       accordion-padding = 0;
-      "focus-follows-mouse".enabled = true;
-      on-focused-monitor-changed = [ ];
-
       gaps = {
         inner = {
           horizontal = 8;
